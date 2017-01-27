0c13fe2ea1855604efadd416c5a97873bdae551f
Kim Turley <kim@bynd.com>
2017-01-27 18:01:49 -0500

    django + database

--- backend/urls.py
+++ backend/urls.py
@@ -22,5 +22,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
